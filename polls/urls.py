9fa0bd61d863dc2317cb05696b6a7d765e3b8fd8
Alina Sansevich <alinasansevich@gmail.com>
2021-07-28 15:41:47 -0500

    First commit.

---
+++ polls/urls.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 28 13:30:46 2021
+
+@author: alina
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    ]
+
+
+
+
+
+
+
